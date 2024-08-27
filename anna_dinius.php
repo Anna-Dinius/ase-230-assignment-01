6574bc16f3ae03a392192d3759f6bead27a71a6b
Anna-Dinius <156371391+Anna-Dinius@users.noreply.github.com>
2024-08-27 14:24:28 -0400

    replaced 'your name' with php

--- anna_dinius.php
+++ anna_dinius.php
@@ -17,8 +17,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Your name's resume">
-	<meta name="author" content="Your name">    
+	<meta name="description" content="<?= $name ?>'s resume">
+	<meta name="author" content="<?= $name ?>">    
 	<link rel="shortcut icon" href="favicon.ico"> 
 	
 	<!-- Google Font -->
